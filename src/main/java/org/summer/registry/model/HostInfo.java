8ada7aa05220c79bef9098df81af03b878581668
linjingfu <jingfu.lin@jyquant.com.cn>
2022-06-14 13:31:21 +0800

    remove registry server(design latter)

--- src/main/java/org/summer/registry/model/HostInfo.java
+++
@@ -1,6 +0,0 @@
-package org.summer.registry.model;
-
-public class HostInfo {
-    private String host;
-    private int port;
-}
--- src/main/java/org/summer/registry/model/SyncRequestCode.java
+++
@@ -1,9 +0,0 @@
-package org.summer.registry.model;
-
-public class SyncRequestCode {
-    //心跳信息
-    public static final int HEARTBEAT = 1;
-
-    //服务端推新的service信息
-    public static final int PUSH_SERVICE_LIST = 10;
-}
